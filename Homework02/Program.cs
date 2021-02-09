dce9ec0f31deed80a665e7a1000d0919d0e3c83c
annabanana59 <annanguyen@stfrancis.edu>
2021-02-08 19:54:17 -0600

    Homework02 Read array of numbers and determine if even or odd

---
+++ Homework02/Program.cs
@@ -0,0 +1,29 @@
+﻿/**
+ * Language:    C#
+ * Author:      Anna Nguyen
+ * Course:      COMP494 - C#
+ * Date:        02/08/2021
+ * Assignment:  Homework #2
+ * Purpose:     Take an array of numbers, loop through the array and output
+ *              the number and state if the number is even or odd
+ */
+using System;
+
+namespace Homework02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 2, 3, 5, 11, 99, 4, 17, 38, 76, 101 };
+
+            for(int i = 0; i < 10; i++)
+            {
+                if (numbers[i] % 2 == 0) // divisible by 2
+                    Console.WriteLine(numbers[i] + " is even.");
+                else // remainder 1
+                    Console.WriteLine(numbers[i] + " is odd.");
+            }
+        }
+    }
+}
